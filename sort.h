bb7263298f61922ba678a6d12f7765693f2929f9
Gbeminiyi <samagbeyigbeminiyi@gmail.com>
2023-02-07 11:14:37 +0000

    Task 3

--- sort.h
+++ sort.h
@@ -22,5 +22,9 @@ void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void swap(int *a, int *b);
+int partition(int *array, int low, int high, int size);
+void sort(int *array, int low, int high, int size);
 
 #endif
